b46649ad70bf5d6dd6344e56a20da283bac9bad7
nerap <44852526+nerap@users.noreply.github.com>
2024-08-27 22:58:20 +0200

    ✨ change ghost to pacman

--- plugins/spaces.sh
+++ plugins/spaces.sh
@@ -7,6 +7,7 @@ source "$HOME/.config/sketchybar/environment.sh"
 ACTIVE_SPACE=$(yabai -m query --spaces --space | jq '.index')
 
 space_bg=(
+  label
   background.color="$SPACEBG"
   background.height=19
   background.corner_radius=50
@@ -19,10 +20,10 @@ for i in "${!SPACE_GHOSTS[@]}"; do
     CURRENT_ICON=$PACMAN
     sketchybar --set space.$sid "${space_bg[@]}"
   else
-    CURRENT_ICON=${SPACE_GHOSTS["$i"]}
+    CURRENT_ICON=$sid
     sketchybar --set space.$sid background.color="$TRANSPARENT"
   fi
 
-  sketchybar --set space.$sid icon="$CURRENT_ICON"
+  sketchybar --set space.$sid icon="$CURRENT_ICON" label="${SPACES[$i]}"
   sketchybar --set current_space icon="${SPACES[$(($ACTIVE_SPACE - 1))]}"
 done
